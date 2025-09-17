df5b8f2f4580a601dc17f50bdb25867e70265a86
Alisson Marques Miquelace <alissonmmiquelace@gmail.com>
2025-09-16 23:46:23 -0300

    add: created avalonia UI project to migrate win forms UI to result in a multplatform application

---
+++ MicrowaveApp.AvaloniaUI/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace MicrowaveApp.AvaloniaUI.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
